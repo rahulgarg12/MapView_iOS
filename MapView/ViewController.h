45ec6f94a1d5b53278049a78aa085ea8a6f1fc69
Rahul Garg <rahulgarg@Rahuls-MacBook.local>
2014-07-28 23:34:07 +0530

    Initial Commit

---
+++ MapView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapView
+//
+//  Created by Rahul Garg on 28/07/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MapView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MapView
+//
+//  Created by Rahul Garg on 28/07/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
